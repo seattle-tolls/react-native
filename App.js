3ac22d2140a79a46ac7c38de8f606b7c07f4be81
ronbarrantes <ronbarrantes@gmail.com>
2019-11-15 00:17:35 -0800

    Get a safe area

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => 
+<View style={styles.container}>
+  <Text>Open up App.js to start working on your app!</Text>
+</View>
+
+
+export default App 
 
 const styles = StyleSheet.create({
   container: {
