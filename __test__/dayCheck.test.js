74bdb9a827a24e8ebe538692ba9d3aa926ac06e3
ronbarrantes <ronbarrantes@gmail.com>
2019-12-30 01:18:25 -0800

    PASS: Test day checker

--- __test__/dayCheck.test.js
+++ __test__/dayCheck.test.js
@@ -1,17 +1,32 @@
 const dayChecker = require('../lib/dayChecker')
 
-describe('Testing the date', () => {
-  it('returns the given date of 2019-12-25 at 6pm', () => {
+describe('DAY CHECKER', () => {
+  it(`checks that today's information is passing without arguments`, () => {
+    const currDate = dayChecker()
+    expect(currDate.dayName).toBeTruthy()
+    expect(typeof currDate.hour).toEqual('number')
+  })
+
+  it('returns the information of Wednesday 2019-12-25 at 6pm', () => {
     const currDate = dayChecker('2019-12-25 18:00')
+    expect(true).toEqual(true)
     expect(currDate.isWeekendOrHoliday).toEqual(true)
     expect(currDate.holidayName).toEqual('Christmas Day')
-    expect(currDate.dayName).toExist('Tuesday')
+    expect(currDate.dayName).toEqual('Wednesday')
     expect(currDate.hour).toEqual(18)
   })
 
-  it('returns the given date of 2019-11-22 at 7am', () => {
-    const currDate = dayChecker('2018-12-25 7:26')
-    expect(currDate.dayName).toExist('Thursday')
+  it('returns the information of Sunday 2019-12-15 at 9am', () => {
+    const currDate = dayChecker('2019-12-15 9:00')
+    expect(currDate.isWeekendOrHoliday).toEqual(true)
+    expect(currDate.holidayName).toEqual(null)
+    expect(currDate.dayName).toEqual('Sunday')
+    expect(currDate.hour).toEqual(9)
+  })
+
+  it('returns the information of Thursday 2019-11-22 at 7am', () => {
+    const currDate = dayChecker('2018-11-22 7:00')
+    expect(currDate.dayName).toEqual('Thursday')
     expect(currDate.isWeekendOrHoliday).toEqual(false)
     expect(currDate.holidayName).toEqual(null)
     expect(currDate.hour).toEqual(7)
