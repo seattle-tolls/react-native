2cdb772be0403fa8635dfd6c615ff345b19ab122
ronbarrantes <ronbarrantes@gmail.com>
2020-01-26 20:18:53 -0800

    Test <CurrentPrice/>

--- __test__/components/current-price.test.js
+++ __test__/components/current-price.test.js
@@ -2,21 +2,25 @@ import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
 
 import { shallow, configure } from 'enzyme'
+import mockDayArr from '../mock/mockDayArr'
 
 configure({ adapter: new Adapter() })
 
 import CurrentPrice from '../../src/components/current-price'
 
-const props = { str:'$3.30' }
-
 describe('<CurrentPrice />', () => {
-  let wrapper
-  beforeEach(() => {
-    wrapper = shallow(<CurrentPrice { ...props }/>)
-  })
+  mockDayArr.map(props => {
+    const { goodToGo, payByMail, schedule, dayName, holidayName } = props
 
-  it('Renders correctly', () => {
-    expect(wrapper.props()).toMatchSnapshot()
-    expect(wrapper.find('Text').contains(props.str)).toBe(true)
+    let wrapper = shallow(<CurrentPrice { ...props }/>)
+    it(`should render component for ${holidayName || dayName}`, () => {
+      expect(wrapper.props()).toMatchSnapshot()
+      expect(wrapper.find('Text').contains(goodToGo)).toBe(true)
+      expect(wrapper.find('Text').contains(payByMail)).toBe(true)
+      expect(wrapper.find('Text').contains(schedule)).toBe(true)
+      expect(wrapper.find('Text').contains(dayName)).toBe(true)
+      holidayName && expect(wrapper.find('Text').contains(holidayName)).toBe(true)
+    })
   })
 })
+
